00b666b6fd16fe15181dbccf23715bdde2ae1e02
din1998 <dinislam8803@gmail.com>
2023-02-16 13:24:44 +0600

    initial-commit

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+export default function NavBar () {
+  return(
+    <div>
+      <Nav>
+        <a>Home</a>
+        <a>Home</a>
+        <a>Home</a>
+        <a>Home</a>
+      </Nav>
+    </div>
+  )
+}
