a689547dceb6e880df4f503f3a0005685987e3f1
din1998 <dinislam8803@gmail.com>
2023-02-16 23:34:55 +0600

    fixe-responsive-issue

--- components/StaticBar.js
+++ components/StaticBar.js
@@ -4,10 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function StatiBar() {
   return(
-    <div className="container mx-auto mt-8">
-      <div className="bg-indigo-400 py-20 rounded-3xl">
-       <div className='flex justify-around'>
-          <div className="flex ">
+    <div className="container mx-auto mt-8 px-4">
+      <div className="bg-indigo-400 py-16 rounded-3xl ">
+       <div className='grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3'>
+          <div className="grid justify-items-center  ">
+            <div className='flex py-8'>
             <Image
             className='backdrop-blur-xl bg-white/30 rounded-md'
             width={50}
@@ -18,29 +19,35 @@ export default function StatiBar() {
             <h1 className='text-white'>Get that 11 line in 30 days</h1>
             <p className='text-slate-300'>Learn more <span className='ml-4'><FontAwesomeIcon icon={faArrowRight} /></span></p>
             </div>
+            </div>
+            
           </div>
-          <div className='flex'>
-          <Image
-            className='backdrop-blur-xl bg-white/30 rounded-md' 
-            width={50}
-            height={50}
-            src='/Vector1.png'
-            />
-            <div className='ml-4 '>
-            <h1 className='text-white'>14 Days sherd challenge</h1>
-            <p className='text-slate-300'>Learn more <span className='ml-12'><FontAwesomeIcon icon={faArrowRight} /></span></p>
+          <div className='grid justify-items-center'>
+            <div className='flex py-8'>
+              <Image
+              className='backdrop-blur-xl bg-white/30 rounded-md' 
+              width={50}
+              height={50}
+              src='/Vector1.png'
+              />
+              <div className='ml-4 '>
+              <h1 className='text-white'>14 Days sherd challenge</h1>
+              <p className='text-slate-300'>Learn more <span className='ml-12'><FontAwesomeIcon icon={faArrowRight} /></span></p>
+              </div>
             </div>
           </div>
-          <div className='flex'>
-          <Image 
-            className='backdrop-blur-xl bg-white/30 rounded-md' 
-            width={50}
-            height={50}
-            src='/Vector2.png'
-            />
-            <div className='ml-4 '>
-            <h1 className='text-white'>Get flat belly in 30 days</h1>
-            <p className='text-slate-300'>Learn more <span className='ml-4'><FontAwesomeIcon icon={faArrowRight} /></span></p>
+          <div className='grid justify-items-center'>
+            <div className='flex py-8'>
+              <Image 
+              className='backdrop-blur-xl bg-white/30 rounded-md' 
+              width={50}
+              height={50}
+              src='/Vector2.png'
+              />
+              <div className='ml-4 '>
+              <h1 className='text-white'>Get flat belly in 30 days</h1>
+              <p className='text-slate-300'>Learn more <span className='ml-4'><FontAwesomeIcon icon={faArrowRight} /></span></p>
+              </div>
             </div>
           </div>
        </div>
